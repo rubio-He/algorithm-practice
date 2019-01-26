34aed4c979877e60074bf1c2aa8f06eb01693a9a
rubio-He <hobbes.ruibohe@gmail.com>
2019-01-26 14:23:00 -0800

    daily update

--- loj/loj100.cc
+++ loj/loj100.cc
@@ -4,23 +4,15 @@ int a[505][505], b[505][505], n, p, m;
 int main() {
 	scanf("%d%d%d", &n,&p,&m);
 	int i, j;
-	for(int i =0; i < n; i++) {
-		for(int j = 0; j < p; j++) {
-			scanf("%d", a[i] + j );
-		}
-	}
-	for(int i = 0; i < p; i++) {
-		for(int j =0; j < m; j++) {
-			scanf("%d", b[i] + j);
-		}
-	}
-	for(int i = 0; i < n; ++i){
-		for(int j = 0; j < m; j++ ){
+	for (i = 0; i < n; ++i)
+		for (j = 0; j < p; ++j) scanf("%d", a[i] + j);
+	for (i = 0; i < p; ++i)
+		for (j = 0; j < m; ++j) scanf("%d", b[i] + j);
+	for (i = 0; i < n; ++i) {
+		for (j = 0; j < m; ++j) {
 			long long s = 0;
-			for(int k = 0; k < p; k++){
-				s += (long long)a[i][k] * b[k][j] % Mod;
-			}
-			printf("%lld ", (s%Mod + Mod)%Mod);
+			for (int k = 0; k < p; ++k) s += (long long)a[i][k] * b[k][j] % Mod;
+			printf("%d ", (s % Mod + Mod) % Mod);
 		}
 		printf("\n");
 	}
