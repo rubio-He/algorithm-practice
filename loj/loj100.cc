84a2c5516743ccb9e441d95534798ba04676e08e
rubio-He <hobbes.ruibohe@gmail.com>
2019-01-26 10:55:02 -0800

    daily update

--- loj/loj100.cc
+++ loj/loj100.cc
@@ -6,20 +6,25 @@ int main() {
 	int i, j;
 	for(int i =0; i < n; i++) {
 		for(int j = 0; j < p; j++) {
-			scanf("%d%d%d", a[i] + j );
-		}}
+			scanf("%d", a[i] + j );
+		}
+	}
 	for(int i = 0; i < p; i++) {
 		for(int j =0; j < m; j++) {
-			scanf("%d%d%d", b[i] + j);
-		}}
+			scanf("%d", b[i] + j);
+		}
+	}
 	for(int i = 0; i < n; ++i){
 		for(int j = 0; j < m; j++ ){
 			long long s = 0;
 			for(int k = 0; k < p; k++){
 				s += (long long)a[i][k] * b[k][j] % Mod;
 			}
-			printf("%d ", (s%Mod + Mod)%Mod);}
-		printf("\n");}
+			printf("%lld ", (s%Mod + Mod)%Mod);
+		}
+		printf("\n");
+	}
+	return 0;
 }
 
 
